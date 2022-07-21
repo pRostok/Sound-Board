4dc9563a9f7f2881947c6595088a8eb7bb41ed41
pRostok <rostmart29@gmail.com>
2022-07-20 17:10:07 -0700

    finalyze project

--- script.js
+++ script.js
@@ -3,27 +3,19 @@ const sounds = ['applause', 'leony', 'libercio', 'libercio2', 'robert', 'stromae
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
-
     btn.innerText = sound
 
     btn.addEventListener('click', () => {
-
         stop()
-
-
         document.getElementById(sound).play()
     })
-
     document.getElementById('buttons').
         appendChild(btn)
 })
 
-
-
 function stop() {
     sounds.forEach(sound => {
         const song = document.getElementById(sound)
-
         song.pause()
         song.currentTime = 0;
     })
